7725889ad45e25f03a4219be4d7033f0b8eef7cf
yamamoto <yamamoto@kyms.jp>
2017-05-28 19:58:51 +0900

    Fix

--- bloginfo/english.php
+++ bloginfo/english.php
@@ -3,8 +3,6 @@ define('_BLOGINFO_Description','
 1, Plug-in to replace <%blogsetting%> in skin and template.<br />
 - ex.) <%BlogInfo(id)%>, <%BlogInfo(short)%>, <%BlogInfo(name)%>, <%BlogInfo(desc)%>, <%BlogInfo(url)%><br />
 2, Plug-in that obtains name and URL etc. of blog specifying blogid for the second paragraph.<br />
-- ex.) <%BlogInfo(id,3)%>, <%BlogInfo(short,3)%>, <%BlogInfo(name,3)%>, <%BlogInfo(desc,3)%>, <%BlogInfo(url,3)%><br />
-3, Plug-in that obtains name and E-Mail, etc. of member specifying memberid for the second paragraph.<br />
-- ex.) <%BlogInfo(mname,1)%>, <%BlogInfo(mrealname,1)%>, <%BlogInfo(mnotes,1)%>, <%BlogInfo(murl,1)%>, <%BlogInfo(memail,1)%>, <%BlogInfo(mid,1)%>
+- ex.) <%BlogInfo(id,3)%>, <%BlogInfo(short,3)%>, <%BlogInfo(name,3)%>, <%BlogInfo(desc,3)%>, <%BlogInfo(url,3)%>
 ');
 ?>
